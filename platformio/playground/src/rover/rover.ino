faf815430624599ee56c3c8c8f714ba5cf13855a
Roman Zaravnyaev <rzaravnyaev@gmail.com>
2023-11-30 13:21:32 +0200

    added alternative wheels layout

--- platformio/playground/src/rover/rover.ino
+++ platformio/playground/src/rover/rover.ino
@@ -3,15 +3,25 @@
 #include <Arduino.h>
 
 
+#define WHEELS_LAYOUT 0
 
+#if WHEELS_LAYOUT
 // GPIO Setting
 extern int gpLb = 12; // Left Wheel Back
 extern int gpLf = 13; // Left Wheel Forward
 extern int gpRb = 15; // Right Wheel Back
 extern int gpRf = 14; // Right Wheel Forward
+
+#else
+
+extern int gpLb = 13; // Left Wheel Back
+extern int gpLf = 12; // Left Wheel Forward
+extern int gpRb = 15; // Right Wheel Back
+extern int gpRf = 14; // Right Wheel Forward
+
+#endif
+
 extern int gpLed =  4; // Light
-
-
 
 void setup() {
   Serial.begin(115200);
