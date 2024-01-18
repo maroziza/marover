724e21976988cb6692074cd4d4c8a1e9f77d358b
Maro Ziza <maro@giver.tech>
2024-01-18 20:23:32 +0200

    arm64 darwin and x86_64 linux is now supported

--- platformio/playground/littlefsbuilder.py
+++ platformio/playground/littlefsbuilder.py
@@ -1,2 +1,9 @@
 Import("env")
-env.Replace( MKFSTOOL=env.get("PROJECT_DIR") + '/mklittlefs' )  # PlatformIO now believes it has actually created a SPIFFS
+import subprocess
+MKFSTOOL=env.get("PROJECT_DIR") + '/tools/mklittlefs.' + subprocess.run(["uname", "-si"],stdout=subprocess.PIPE).stdout.decode().replace(' ','.').rstrip();
+env.Replace(MKFSTOOL=MKFSTOOL);
+print("Using: "+MKFSTOOL);
+#
+# PlatformIO now believes it has actually created a SPIFFS
+#
+
