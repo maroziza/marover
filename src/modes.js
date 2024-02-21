56de8fff349875eea0f3c03590c6348777422f51
Maro Ziza <maro@giver.tech>
2024-02-22 01:26:24 +0200

    icon font changed

--- src/modes.js
+++ src/modes.js
@@ -6,7 +6,7 @@ import {I2Cbus} from "drivers/i2c.js";
 import Helvetica from "fonts/TimesMR8.json";
 import Bold from "fonts/HelveticaBR8.json";
 import Schoolbook from "fonts/TimesBR14.json";
-import Sm4tik from 'fonts/sm4tikMR16.json'
+import Icons from 'fonts/SijiMR10.json'
 
 
 // SSD1306.find(i2c) // try to find by default addresses
@@ -16,14 +16,15 @@ screen = new SSD1306(i2c.device(0x3c), 128,64, false),
 relays = new PCF8574(i2c.device(0x20));
 
 var regular = screen.prepareFont(Helvetica, 1, 0, 1);
-var icons8 = screen.prepareFont(Sm4tik, 1, 0, 4);
+var icons8 = screen.prepareFont(Icons, 1, 0, 2, 1);
 Object.assign(regular, regular, icons8);
 
 var bold = screen.prepareFont(Bold,1,0,1,1);
 
 var roman = screen.prepareFont(Schoolbook, 2 ,0x8001, 1);
-var icons16 = screen.prepareFont(Sm4tik, 2, 0x8001, 0);
+var icons16 = screen.prepareFont(Icons, 2, 0x8001, -2);
 Object.assign(roman, icons16, roman);
+
 
 roman.lines=2;
 var
@@ -42,9 +43,9 @@ screen.init();
 screen.gotoPage(roman, 0);
 screen.drawLetters(roman, "Hello \uE015bramfaktura!!")
 screen.gotoPage(regular, 2);
-screen.drawLetters(regular, "Never \uE015 underestimate your enemy please ");
+/**screen.drawLetters(regular, "Never \uE015 underestimate your enemy please ");
 screen.drawLetters(bold,"never");
-//screen.drawLetters(regular," forgive forget be vigilant. привітики вам з криївки передає япручок ґї       \ue006");
-screen.drawLetters(regular, JSON.stringify(screen));
-
+screen.drawLetters(regular," forgive forget be vigilant. привітики вам з криївки передає япручок ґї       \ue006");
+*/
+screen.showFont(icons8);
 
